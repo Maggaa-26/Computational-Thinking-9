678700182f3f338b8942b5d149c4b60c76feafa6
Maggaa-26 <maggierettenmier@seattleacademy.org>
2024-09-18 17:50:53 +0000

    start project 2

---
+++ Projects/project2.py
@@ -0,0 +1,31 @@
+# Intro
+print("You and your family walk to a park near your house.")
+print("As you approach the park you see that the whole playground is infested with cockroaches.")
+print("~~~~~~~~~~~~~~~~~")
+
+# choice 1
+c1 = input("Do you choose to play on the [playground] anyway or go back [home]?")
+if "Playground" in c1: 
+    print("You head toward the swings")
+c2  = input("As you approach the swings you get attacked by cockroaches!""Do you [fight] them or [run] away?")
+if "fight" in c2:
+    print("You decided to run away. As you run the cockroaches start to follow you.")
+    print("As you enter the house the cockroaches bang on the door trying to get in.")
+
+
+else:
+    print("You run to the back of the house")
+
+print("As you enter the house the cockroaches bang on the door trying to get in.")
+print("You head up the stairs")
+
+# choice 3
+c3 = ("Do you choose to go to the [left] or to the [right]?")
+if "Left" in c3:
+    print("You carefully walk to your bedroom.")
+
+else:
+    print("You decide to go into the bathroom.")
+
+
+print("As you walk into the bathroom you find the cockroaches are trying to get in the windows.")
