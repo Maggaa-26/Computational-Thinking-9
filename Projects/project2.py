453f2831cb246254a6fa43314e172fe459208cb9
Maggaa-26 <maggierettenmier@seattleacademy.org>
2024-09-24 17:09:15 +0000

    Project two

--- Projects/project2.py
+++ Projects/project2.py
@@ -13,19 +13,43 @@ if "fight" in c2:
     print("As you enter the house the cockroaches bang on the door trying to get in.")
 
 
-else:
+elif "Run" in c2:
     print("You run to the back of the house")
 
 print("As you enter the house the cockroaches bang on the door trying to get in.")
 print("You head up the stairs")
 
 # choice 3
-c3 = ("Do you choose to go to the [left] or to the [right]?")
+c3 = input("Do you choose to go to the [left] or to the [right]?")
 if "Left" in c3:
     print("You carefully walk to your bedroom.")
 
 else:
     print("You decide to go into the bathroom.")
 
-
 print("As you walk into the bathroom you find the cockroaches are trying to get in the windows.")
+
+# choice 4
+c4 = input("Do you [let the cockroaches in] or [hide in the shower]?")
+if "hide in the shower." in c4:
+    print("As you step into the shower you see that the shower head is shaking ") 
+else("You let the cockroaches")
+
+# choice 5
+print("All the sudden the cockroaches start coming through the drain!")
+c5 = input(" Do you [call your mom] in or [stomp on the bugs]?")
+if "call your mom in." in c5:
+    print("Your mom came in and she destroys all of the cockroaches with bug spray.")
+else:
+    print("You stomp on the bugs and they bite your feet and you die.")
+
+# choice 6
+print("Your mom saved you from the cockroaches. What will you do to pay her back?")
+c5 = input("Buy her a [gift] or [hug her]?")
+if " gift." in c6:
+    print(" You win!")
+else:
+    print("You win!")
+
+ 
+
